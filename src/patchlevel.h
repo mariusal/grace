62e1f1d0f1ff1a7f9c10d53c3f91640f3caed9aa
fnevgeny <fnevgeny>
1999-01-31 00:15:06 +0000

    grace-990131 snapshot

--- src/patchlevel.h
+++ src/patchlevel.h
@@ -32,7 +32,7 @@
 #define MAJOR_REV 5
 #define MINOR_REV 0
 #define PATCHLEVEL 2
-#define BETA_VER "(990113)"
+#define BETA_VER "(990131)"
 
 #define VERSION_ID (MAJOR_REV*10000 + MINOR_REV*100 + PATCHLEVEL)
 
