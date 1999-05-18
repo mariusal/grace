7af0f5e83eaf93bbfc41831cf91c8cbf2a4d9971
fnevgeny <fnevgeny>
1999-05-18 22:31:34 +0000

    grace-990519 snapshot

--- src/buildinfo.c
+++ src/buildinfo.c
@@ -16,7 +16,7 @@
 #define MAJOR_REV 5
 #define MINOR_REV 0
 #define PATCHLEVEL 3
-#define BETA_VER "(990517)"
+#define BETA_VER "(990519)"
 
 #ifndef GRACE_HOME
 #  define GRACE_HOME "/usr/local/grace"
