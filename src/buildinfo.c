ab7c1b2fac66b6977f16321e9722dd6e891f6558
fnevgeny <fnevgeny>
1999-06-14 01:18:11 +0000

    grace-990614

--- src/buildinfo.c
+++ src/buildinfo.c
@@ -16,7 +16,7 @@
 #define MAJOR_REV 5
 #define MINOR_REV 0
 #define PATCHLEVEL 3
-#define BETA_VER "(990602)"
+#define BETA_VER "(990614)"
 
 #ifndef GRACE_HOME
 #  define GRACE_HOME "/usr/local/grace"
