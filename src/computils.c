6aeda12cd7e419c51613077071acf1eae8d63a3e
fnevgeny <fnevgeny>
1999-07-10 22:49:39 +0000

    Use copysetdata() instead of copyset() in do_compute()

--- src/computils.c
+++ src/computils.c
@@ -1,10 +1,10 @@
 /*
- * Grace - Graphics for Exploratory Data Analysis
+ * Grace - GRaphing, Advanced Computation and Exploration of data
  * 
  * Home page: http://plasma-gate.weizmann.ac.il/Grace/
  * 
  * Copyright (c) 1991-95 Paul J Turner, Portland, OR
- * Copyright (c) 1996-98 GRACE Development Team
+ * Copyright (c) 1996-99 Grace Development Team
  * 
  * Maintained by Evgeny Stambulchik <fnevgeny@plasma-gate.weizmann.ac.il>
  * 
@@ -79,7 +79,7 @@ int do_compute(int gno, int setno, int graphto, int loadto, char *rarray, char *
 {
     if (is_set_active(gno, setno)) {
 	if (gno != graphto || setno != loadto) {
-	    if (copyset(gno, setno, graphto, loadto) != GRACE_EXIT_SUCCESS) {
+	    if (copysetdata(gno, setno, graphto, loadto) != GRACE_EXIT_SUCCESS) {
 	        return GRACE_EXIT_FAILURE;
             }
         }
