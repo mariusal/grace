78eef27ab820a3a5c373efc9e75a5ec9747681b4
fnevgeny <fnevgeny>
1999-10-05 21:26:47 +0000

    Added set/get_max_path_limit() functions to control the maximum path length allowed for polyline/polygon drawings.

--- src/draw.h
+++ src/draw.h
@@ -1,10 +1,10 @@
 /*
- * Grace - Graphics for Exploratory Data Analysis
+ * Grace - GRaphing, Advanced Computation and Exploration of data
  * 
  * Home page: http://plasma-gate.weizmann.ac.il/Grace/
  * 
  * Copyright (c) 1991-95 Paul J Turner, Portland, OR
- * Copyright (c) 1996-98 GRACE Development Team
+ * Copyright (c) 1996-99 Grace Development Team
  * 
  * Maintained by Evgeny Stambulchik <fnevgeny@plasma-gate.weizmann.ac.il>
  * 
@@ -240,5 +240,8 @@ int number_of_linestyles(void);
 
 void vpswap(VPoint *vp1, VPoint *vp2);
 int points_overlap(VPoint vp1, VPoint vp2);
+
+void set_max_path_limit(int limit);
+int get_max_path_limit(void);
 
 #endif /* __DRAW_H_ */
