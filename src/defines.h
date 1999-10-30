121af002e5afa30e332be1c1342b3dd664d52bde
fnevgeny <fnevgeny>
1999-10-30 00:09:47 +0000

    More generic symtab_entry.

--- src/defines.h
+++ src/defines.h
@@ -1,10 +1,10 @@
 /*
- * Grace - Graphics for Exploratory Data Analysis
+ * Grace - GRaphing, Advanced Computation and Exploration of data
  * 
  * Home page: http://plasma-gate.weizmann.ac.il/Grace/
  * 
  * Copyright (c) 1991-95 Paul J Turner, Portland, OR
- * Copyright (c) 1996-98 GRACE Development Team
+ * Copyright (c) 1996-99 Grace Development Team
  * 
  * Maintained by Evgeny Stambulchik <fnevgeny@plasma-gate.weizmann.ac.il>
  * 
@@ -388,7 +388,7 @@ typedef enum {
 typedef struct {
     char *s;
     int type;
-    double (*fnc)();
+    void *data;
 } symtab_entry;
 
 /*
