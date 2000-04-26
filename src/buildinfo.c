f1e66b1c551948e01cbf42e0bda2b719098f379f
fnevgeny <fnevgeny>
2000-04-26 22:42:07 +0000

    grace-20000427 snapshot.

--- src/buildinfo.c
+++ src/buildinfo.c
@@ -16,7 +16,7 @@
 #define MAJOR_REV 5
 #define MINOR_REV 1
 #define PATCHLEVEL 1
-#define BETA_VER "(20000419)"
+#define BETA_VER "(20000427)"
 
 #ifndef GRACE_HOME
 #  define GRACE_HOME "/usr/local/grace"
