c3d92c0fda614d33742280eb6b9497133d7beaaa
fnevgeny <fnevgeny>
2000-05-27 22:33:22 +0000

    Renamed create_monitor_frame() -> create_monitor_frame_cb(). Set the console height via Xresources, just as the width. Added an option to popup the console only on errors.

--- src/monwin.c
+++ src/monwin.c
@@ -46,6 +46,7 @@
 
 
 static void clear_results(void *data);
+static void popup_on(int onoff, void *data);
 static void create_wmon_frame(void *data);
 static int wmon_apply_notify_proc(void *data);
 
@@ -55,24 +56,25 @@ typedef struct _console_ui
     Widget monText;
     Widget wmon_frame;
     Widget wmon_text_item;
+    int popup_only_on_errors;
 } console_ui;
 
 /*
  * Create the mon Panel
  */
-void create_monitor_frame(void *data)
+static void create_monitor_frame(int force, char *msg)
 {
     static console_ui *ui = NULL;
-    char *msg = (char *) data;
     
     set_wait_cursor();
 
     if (ui == NULL) {
-        Widget menubar, menupane, wbut, fr;
+        Widget menubar, menupane, fr;
 
 	ui = xmalloc(sizeof(console_ui));
         ui->mon_frame = CreateDialogForm(app_shell, "Console");
         ui->wmon_frame = NULL;
+        ui->popup_only_on_errors = FALSE;
 
         menubar = CreateMenuBar(ui->mon_frame);
         ManageChild(menubar);
@@ -81,23 +83,22 @@ void create_monitor_frame(void *data)
         menupane = CreateMenu(menubar, "File", 'F', FALSE);
         CreateMenuButton(menupane, "Save...", 'S', create_wmon_frame, ui);
         CreateMenuSeparator(menupane);
-        wbut = CreateMenuButton(menupane,
-            "Close", 'C', destroy_dialog_cb, GetParent(ui->mon_frame));
-        XtVaSetValues(ui->mon_frame, XmNcancelButton, wbut, NULL);
+        CreateMenuCloseButton(menupane, ui->mon_frame);
         
         menupane = CreateMenu(menubar, "Edit", 'E', FALSE);
         CreateMenuButton(menupane, "Clear", 'C', clear_results, ui);
+
+        menupane = CreateMenu(menubar, "Options", 'O', FALSE);
+        CreateMenuToggle(menupane, "Popup only on errors", 'e', popup_on, ui);
 
         menupane = CreateMenu(menubar, "Help", 'H', TRUE);
         CreateMenuButton(menupane, "On console", 'c', HelpCB, NULL);
 	
         fr = CreateFrame(ui->mon_frame, NULL);
         
-	ui->monText = CreateScrollTextItem2(fr, 5, "Messages:");
+	ui->monText = CreateScrollTextItem2(fr, 0, "Messages:");
         XmTextSetString(ui->monText, "");
-        XtVaSetValues(ui->monText,
-            XmNeditable, False,
-            NULL);
+        XtVaSetValues(ui->monText, XmNeditable, False, NULL);
 
         AddDialogFormChild(ui->mon_frame, fr);
         ManageChild(ui->mon_frame);
@@ -109,9 +110,17 @@ void create_monitor_frame(void *data)
         XmTextInsert(ui->monText, pos, msg);
     }
     
-    RaiseWindow(GetParent(ui->mon_frame));
+    if (force || ui->popup_only_on_errors == FALSE) {
+        RaiseWindow(GetParent(ui->mon_frame));
+    }
     
     unset_wait_cursor();
+}
+
+static void popup_on(int onoff, void *data)
+{
+    console_ui *ui = (console_ui *) data;
+    ui->popup_only_on_errors = onoff;
 }
 
 static void clear_results(void *data)
@@ -177,7 +186,7 @@ static int wmon_apply_notify_proc(void *data)
 
 void stufftextwin(char *msg)
 {
-    create_monitor_frame(msg);
+    create_monitor_frame(FALSE, msg);
 }
 
 void errwin(char *msg)
@@ -188,7 +197,12 @@ void errwin(char *msg)
     buf = concat_strings(buf, msg);
     buf = concat_strings(buf, "\n");
     
-    stufftextwin(buf);
+    create_monitor_frame(TRUE, buf);
     
     xfree(buf);
+}
+
+void create_monitor_frame_cb(void *data)
+{
+    create_monitor_frame(TRUE, NULL);
 }
