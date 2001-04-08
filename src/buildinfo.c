3212bb983622edbc73f40c14d46dc784a48a913e
fnevgeny <fnevgeny>
2001-04-08 14:02:46 +0000

    Final 5.1.3 release.

--- src/buildinfo.c
+++ src/buildinfo.c
@@ -18,7 +18,7 @@
 #define MAJOR_REV 5
 #define MINOR_REV 1
 #define PATCHLEVEL 3
-#define BETA_VER "(rc2)"
+/* #define BETA_VER "(rc2)" */
 
 #ifndef GRACE_HOME
 #  define GRACE_HOME "/usr/local/grace"
