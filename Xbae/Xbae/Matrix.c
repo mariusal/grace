a3e582b955ea045a048562058f85e9f68388546a
fnevgeny <fnevgeny>
2004-07-02 21:26:47 +0000

    Bind "Ctrl <Key>osfCancel" instead of <Key>osfCancel to CancelEdit.

--- Xbae/Xbae/Matrix.c
+++ Xbae/Xbae/Matrix.c
@@ -22,7 +22,7 @@
  *
  * MatrixWidget Author: Andrew Wason, Bellcore, aw@bae.bellcore.com
  *
- * $Id: Matrix.c,v 1.2 2001-12-26 20:46:48 fnevgeny Exp $
+ * $Id: Matrix.c,v 1.3 2004-07-02 21:26:47 fnevgeny Exp $
  */
 
 #ifdef HAVE_CONFIG_H
@@ -78,7 +78,7 @@ Shift ~Ctrl ~Meta ~Alt <Key>Tab	:	EditCell(Left)\n\
 <Key>osfDown			:	EditCell(Down)\n\
 <Key>osfActivate		:	CommitEdit(False)\n\
 ~Shift ~Meta ~Alt <Key>Return	:   	CommitEdit(False)\n\
-<Key>osfCancel			:	CancelEdit(False)\n\
+Ctrl <Key>osfCancel		:	CancelEdit(False)\n\
 Shift Ctrl ~Meta ~Alt <Key>Tab	:	TraversePrev()\n\
 Ctrl ~Meta ~Alt <Key>Tab	:	TraverseNext()\n\
 <Key>osfPageDown		:	PageDown()\n\
