e7b6b42027ec140a23b9aeff3f5e5c3c72a1e44b
fnevgeny <fnevgeny>
2005-05-19 20:41:26 +0000

    Copyright notices updated.

--- src/helpwin.c
+++ src/helpwin.c
@@ -163,7 +163,7 @@ void create_about_grtool(void *data)
 	fr = CreateFrame(about_panel, "Legal stuff");
         rc = CreateVContainer(fr);
 	CreateLabel(rc, "Copyright (c) 1991-1995 Paul J Turner");
-	CreateLabel(rc, "Copyright (c) 1996-2004 Grace Development Team");
+	CreateLabel(rc, "Copyright (c) 1996-2005 Grace Development Team");
 	CreateLabel(rc, "Maintained by Evgeny Stambulchik");
 	CreateLabel(rc, "All rights reserved");
 	CreateLabel(rc,
