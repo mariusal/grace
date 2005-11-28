b5cb442debe3e60b57fe31847ad0ae0cc82e7304
fnevgeny <fnevgeny>
2005-11-28 21:59:28 +0000

    5.1.20 started.

--- src/buildinfo.c
+++ src/buildinfo.c
@@ -26,8 +26,8 @@
 
 #define MAJOR_REV 5
 #define MINOR_REV 1
-#define PATCHLEVEL 19
-/* #define BETA_VER "rc1" */
+#define PATCHLEVEL 20
+#define BETA_VER "dev"
 
 #ifndef GRACE_HOME
 #  define GRACE_HOME "/usr/local/grace"
