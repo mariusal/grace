92a8edc70f47f632e4ec3fd1c1ac91a67bceb7e2
fnevgeny <fnevgeny>
2006-08-20 23:40:03 +0000

    Removed a few time includes.

--- src/utils.h
+++ src/utils.h
@@ -29,21 +29,7 @@
 #ifndef __UTILS_H_
 #define __UTILS_H_
 
-/* for time_t */
-#ifdef TIME_WITH_SYS_TIME
-#  include <sys/time.h>
-#  include <time.h>
-#else
-#  ifdef HAVE_SYS_TIME_H
-#    include <sys/time.h>
-#  else
-#    include <time.h>
-#  endif
-#endif
-
 #include "graceapp.h"
-
-#define on_or_off(x) ((x)?"on":"off")
 
 int argmatch(char *s1, char *s2, int atleast);
 void lowtoupper(char *s);
