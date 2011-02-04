90cd792be5af81cfd910a090c00f46f2290ea010
Marius <mariausol@gmail.com>
2011-02-04 16:04:24 +0200

    Got rid of canvas events

--- src/qtgui/canvaswidget.h
+++ src/qtgui/canvaswidget.h
@@ -2,24 +2,6 @@
 #define __CANVASWIDGET_H_
 
 #include <QWidget>
-#include <QLabel>
-
-extern "C" {
-#include <globals.h>
-#include <grace/grace.h>
-#include <graceapp.h>
-#include <core_utils.h>
-#include <xprotos.h>
-}
-
-typedef struct {
-    VPoint vp;
-    int include_graphs;
-    Quark *q;
-    int part;
-    view bbox;
-    int found;
-} canvas_target;
 
 class CanvasWidget : public QWidget
 {
@@ -27,11 +9,7 @@ class CanvasWidget : public QWidget
 
 public:
     CanvasWidget(QWidget *parent = 0);
-    void set_action(GUI *gui, unsigned int npoints, int seltype,
-        CanvasPointSink sink, void *data);
-
     QImage *pixmap;
-
 
 protected:
     void mouseMoveEvent(QMouseEvent *event);
@@ -44,29 +22,7 @@ protected:
     void contextMenuEvent(QContextMenuEvent *event);
 
 private:
-    X11Stuff *xstuff;
-
-    int region_need_erasing;
-    int last_b1down_x, last_b1down_y;   /* coords of last event */
-    int last_mouse_move_x, last_mouse_move_y;
-    int undo_point;
-    int abort_action;
-    canvas_target ct;
-    int on_focus;
-
-    void qt_VPoint2dev(const VPoint *vp, double *x, double *y);
-    void qt_dev2VPoint(double x, double y, VPoint *vp);
-    Quark* next_graph_containing(Quark *q, VPoint *vp);
     void paintEvent(QPaintEvent *event);
-
-    void completeAction(int x, int y);
-
-    Grace *grace;
-    Canvas *canvas;
-    GProject *gp;
-    int hdevice;
-    GrFILE *grf;
-    //FILE *fpout;
 };
 
 #endif /* __CANVASWIDGET_H_ */
