797e50600329b981bdccce9a92b15cc6c67d68af
Marius <mariausol@gmail.com>
2011-02-23 10:26:12 +0200

    Updated startup script

--- auxiliary/rungrace.bat
+++ auxiliary/rungrace.bat
@@ -1,6 +1,6 @@
-set GRACE_HOME=%CD%
+set GRACE_HOME=%CD%\grace
 set GRACE_EDITOR=notepad
 set GRACE_PRINT_CMD=print /d:LTP1
 set GRACE_HELPVIEWER="@start "" /b %GRACE_HOME%\runhelpviewer.bat"
 set HOME=%HOMEDRIVE%%HOMEPATH%
-start bin\qtgrace.exe
+start %GRACE_HOME%\bin\qtgrace.exe
