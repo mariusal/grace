9533dd11ce9f0235bdf4ffc35ab100f2e38c9b31
Marius <mariausol@gmail.com>
2011-04-11 17:17:03 +0300

    Fixed issues with a white space in paths

--- auxiliary/rungrace.bat
+++ auxiliary/rungrace.bat
@@ -1,6 +1,7 @@
-set GRACE_HOME=%CD%\grace
+set GRACE_HOME=%~dp0\grace
 set GRACE_EDITOR=notepad
 set GRACE_PRINT_CMD=print /d:LTP1
-set GRACE_HELPVIEWER="@start "" /b %GRACE_HOME%\runhelpviewer.bat"
+set GRACE_HELPVIEWER="cd %GRACE_HOME% & @start "" /b runhelpviewer.bat"
 set HOME=%HOMEDRIVE%%HOMEPATH%
-start %GRACE_HOME%\bin\qtgrace.exe
+cd %GRACE_HOME%
+@start bin\qtgrace.exe %1
