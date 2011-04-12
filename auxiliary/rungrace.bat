ebef7230ff360f6633835b348bfcef0e6e595492
Marius <mariausol@gmail.com>
2011-04-12 16:19:36 +0300

    Set working directory to %CD% and fixed issues with spaces in paths for help viewer

--- auxiliary/rungrace.bat
+++ auxiliary/rungrace.bat
@@ -1,7 +1,6 @@
-set GRACE_HOME=%~dp0\grace
+set GRACE_HOME=%~dp0grace
 set GRACE_EDITOR=notepad
 set GRACE_PRINT_CMD=print /d:LTP1
-set GRACE_HELPVIEWER="cd %GRACE_HOME% & @start "" /b runhelpviewer.bat"
+set GRACE_HELPVIEWER="cd %GRACE_HOME% & @start "" /b runhelpviewer.bat "%%s""
 set HOME=%HOMEDRIVE%%HOMEPATH%
-cd %GRACE_HOME%
-@start bin\qtgrace.exe %1
+@start "" "%GRACE_HOME%\bin\qtgrace.exe" -wd "%CD%" %1
