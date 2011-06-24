afd0448bdc0102d28858bb5b0aa8db187f092b74
Marius <mariausol@gmail.com>
2011-06-25 00:14:49 +0300

    Fixed \${filename} on windows again

--- auxiliary/rungrace.bat
+++ auxiliary/rungrace.bat
@@ -5,8 +5,8 @@ set GRACE_HELPVIEWER="cd %GRACE_HOME% & @start "" /b runhelpviewer.bat "%%s""
 set HOME=%HOMEDRIVE%%HOMEPATH%
 set GRACE_HOME=%GRACE_HOME:\=/%
 set CD=%CD:\=/%
-IF [%1] NEQ [] (
-  set FILE_PATH=%1
+set FILE_PATH=%1
+IF [%FILE_PATH%] NEQ [] (
   set FILE_PATH=%FILE_PATH:\=/%
 )
 @start "" "%GRACE_HOME%/bin/qtgrace.exe" -wd "%CD%" %FILE_PATH%
