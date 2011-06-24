f959d42dc2d309d06f4a2c295ef9427807f5aeff
Marius <mariausol@gmail.com>
2011-06-25 00:04:00 +0300

    More fixes for \${filename} on windows

--- auxiliary/rungrace.bat
+++ auxiliary/rungrace.bat
@@ -3,6 +3,10 @@ set GRACE_EDITOR=notepad
 set GRACE_PRINT_CMD=print /d:LTP1
 set GRACE_HELPVIEWER="cd %GRACE_HOME% & @start "" /b runhelpviewer.bat "%%s""
 set HOME=%HOMEDRIVE%%HOMEPATH%
-set FILE_PATH=%1
-set FILE_PATH=%FILE_PATH:\=/%
-@start "" "%GRACE_HOME%\bin\qtgrace.exe" -wd "%CD%" %FILE_PATH%
+set GRACE_HOME=%GRACE_HOME:\=/%
+set CD=%CD:\=/%
+IF [%1] NEQ [] (
+  set FILE_PATH=%1
+  set FILE_PATH=%FILE_PATH:\=/%
+)
+@start "" "%GRACE_HOME%/bin/qtgrace.exe" -wd "%CD%" %FILE_PATH%
