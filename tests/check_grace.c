32261c8d851479049196a7b253f2f073c6af0885
Marius <mariausol@gmail.com>
2011-12-31 00:07:12 +0200

    Added a sample test file

---
+++ tests/check_grace.c
@@ -0,0 +1,12 @@
+#include "gtest/gtest.h"
+
+TEST(TableWidgetTest, TableValidator) {
+  int i = 1;
+  EXPECT_EQ(1, i);
+}
+
+TEST(TreeWidgetTest, TreeValidator) {
+  int j = 2;
+  EXPECT_EQ(2, j);
+}
+
